2a220bd6299b65df6b53bd86d12352b8a902ac87
Gabriela <gabrielamgp01@gmail.com>
2026-09-22 19:15:16 -0300

    'Feat: Inclusão da coluna N_AIH no rd_colunas'

--- pipeline/02_transformacao.py
+++ pipeline/02_transformacao.py
@@ -68,7 +68,7 @@ def consolidar_arquivos_brutos() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFra
     # Lendo os arquivos rd.parquet e concatenando em um DataFrame, apenas com as colunas que nos interessam
     # ------------------------------------------------------------------
     rd_colunas = ['UF_ZI', 'ANO_CMPT', 'MES_CMPT', 'ESPEC', 'CGC_HOSP', 'MUNIC_MOV', 'QT_DIARIAS',
-                  'DT_INTER', 'DT_SAIDA', 'DIAS_PERM', 'CNES']
+                  'DT_INTER', 'DT_SAIDA', 'DIAS_PERM', 'CNES', 'N_AIH', 'IDENT']
     df_rd = ler_consolidado(arquivos_rd, rd_colunas)
     logger.info(f"Arquivos RD encontrados: {len(arquivos_rd)}")
 
@@ -84,7 +84,9 @@ def consolidar_arquivos_brutos() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFra
         'DT_INTER': 'string',
         'DT_SAIDA': 'string',
         'DIAS_PERM': 'Int64',
-        'CNES': 'string'
+        'IDENT': 'string',
+        'CNES': 'string',
+        'N_AIH': 'string'
     })
     
     # ------------------------------------------------------------------
