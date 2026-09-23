196468cb8ac065af1aa84121567a62dc83b07464
Vandelson Elias Monteiro Filho <vandelson.elias@gmail.com>
2026-09-22 21:31:00 -0300

    Inclusao da coluna CNES na tabela RD

--- pipeline/02_transformacao.py
+++ pipeline/02_transformacao.py
@@ -68,7 +68,7 @@ def consolidar_arquivos_brutos() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFra
     # Lendo os arquivos rd.parquet e concatenando em um DataFrame, apenas com as colunas que nos interessam
     # ------------------------------------------------------------------
     rd_colunas = ['UF_ZI', 'ANO_CMPT', 'MES_CMPT', 'ESPEC', 'CGC_HOSP', 'MUNIC_MOV', 'QT_DIARIAS',
-                  'DT_INTER', 'DT_SAIDA', 'DIAS_PERM']
+                  'DT_INTER', 'DT_SAIDA', 'DIAS_PERM', 'CNES']
     df_rd = ler_consolidado(arquivos_rd, rd_colunas)
     logger.info(f"Arquivos RD encontrados: {len(arquivos_rd)}")
 
@@ -83,7 +83,8 @@ def consolidar_arquivos_brutos() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFra
         'QT_DIARIAS': 'Int64',
         'DT_INTER': 'string',
         'DT_SAIDA': 'string',
-        'DIAS_PERM': 'Int64'
+        'DIAS_PERM': 'Int64',
+        'CNES': 'string'
     })
     
     # ------------------------------------------------------------------
